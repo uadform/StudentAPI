6688047dbb02d1390ef46543ee110018874a033f
Ugnius Adomavičius <u.adomavicius@adform.com>
2023-12-08 15:55:17 +0200

    indexes

---
+++ StudentAPI/Scripts/IndexingScript.sql
@@ -0,0 +1,16 @@
+﻿-- Index on the primary key
+CREATE INDEX idx_student_id ON Student (student_id);
+
+-- Index on the foreign key
+CREATE INDEX idx_student_department_id ON Student (department_id);
+
+-- Index on the primary key
+CREATE INDEX idx_lecture_id ON Lecture (lecture_id);
+
+-- Index on the foreign keys
+CREATE INDEX idx_department_lecture_department_id ON DepartmentLecture (department_id);
+CREATE INDEX idx_department_lecture_lecture_id ON DepartmentLecture (lecture_id);
+
+-- Index on the primary key
+CREATE INDEX idx_department_id ON Department (department_id);
+
